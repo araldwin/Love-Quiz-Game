33db54946a0785de4fede08a89d76cce33dc699a
araldwin <araldwin@gmail.com>
2022-10-06 20:35:49 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+name = input('Enter your username: ')
+
+while True:
+    print('Are you ready to answer the quiz ' + name + '?')
+    cont = input("(yes/no?): ")
+    while cont.lower() not in ("yes", "no"):
+        cont = input("please answer only (yes/no?):")
+    
+    if cont == "yes":
+        print("Proceeding to the first question!!!")
+        quit()
+    else:
+        continue
+
