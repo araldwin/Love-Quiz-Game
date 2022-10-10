82a66514a7f8d6f42580a4658db7fc9c1670e9a9
araldwin <araldwin@gmail.com>
2022-10-10 13:54:23 +0000

    Add quiz a display question function

--- quiz.py
+++ quiz.py
@@ -74,46 +74,56 @@ QUESTIONS = [{
     'correct_option': '2'
 }]
 
+questionIndex = 0
+score = 0
 
-def display_question():
+
+def display_question(q):
     """
         1. display question to the user
         2. display options to the user
     """
+    print(q['question'])
+    
+    
+
+# def ask_user_option():
+#     """
+#     3. ask user for in put 
+#     4. validate user input 
+#     5. if user input is invalid then ask user for input again
+#     """
 
 
-def ask_user_option():
-    """
-    3. ask user for in put 
-    4. validate user input 
-    5. if user input is invalid then ask user for input again
-    """
+# def check_user_answer():
+
+#     """
+#     6. if user input is valid check input with correct answer
+#     7. if user input == correct_option of the question then increment score
+#     """
 
 
-def check_user_answer():
+# def display_question_result():
 
-    """
-    6. if user input is valid check input with correct answer
-    7. if user input == correct_option of the question then increment score
-    """
-
-
-def display_question_result():
-"""
-8. display message to user 
-9. increment questionIndex
-"""
+#     """
+#     8. display message to user 
+#     9. increment questionIndex
+#     """
 
 
-def check_and_display_final_score():
- # if questionIndex == size of the QUESTIONS array then display final score of the user and exit
+# def check_and_display_final_score():
+#  # if questionIndex == size of the QUESTIONS array then display final score of the user and exit
 
 
-def play_again():
-# ask user to quit or play again
+# def play_again():
+# # ask user to quit or play again
 
 
 def main():
     """
     create loop for all the function above
     """
+    for question in QUESTIONS:
+        display_question(question)
+
+main()
