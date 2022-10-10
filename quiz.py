7d806aa56067dfeb26d206e36780c0118a148661
araldwin <araldwin@gmail.com>
2022-10-10 13:36:04 +0000

    Add QUESTIONS list dictionary to quiz

--- quiz.py
+++ quiz.py
@@ -21,6 +21,59 @@ while True:
     else:
         pass
 
+QUESTIONS = [{
+    'question': 'In which Italian city can you find the Colosseum?',
+    'options': '1. Venice\n2. Rome\n3. Naples\n4. Milan',
+    'correct_option': '2'
+},
+     {
+    'question': 'In the Big Bang Theory Tv series, what is the name of Sheldon and Leonard’s neighbor?',
+    'options': '1. Penny\n2. Patty\n3. Lily\n4. Jessie',
+    'correct_option': '1'
+},
+     {
+    'question': 'In which country was the airline Ryanair founded?',
+    'options': '1. Germany\n2. Scotland\n3. Ireland\n4. Spain',
+    'correct_option': '3'
+},
+     {
+    'question': 'When was the first Harry Potter book published?',
+    'options': '1. 1997\n2. 1999\n3. 2001\n4. 2003',
+    'correct_option': '1'
+},
+     {
+    'question': '“When I find myself in times of trouble, Mother Mary comes to me” is the opening line of which song?',
+    'options': '1. Smells like teen spirit – Nirvana\n2. Get lucky – Daft Punk\n'
+               '3. Sweet Child O’ Mine – Gun N’ Roses\n4. Let it be – The Beatles',
+    'correct_option': '4'
+},
+     {
+    'question': 'Who famously said “Veni, vidi, vici”?',
+    'options': '1. Winston Churchill\n2. Charles de Gaulle\n3. Julius Caesar\n4. Alexander the Great',
+    'correct_option': '3'
+},
+     {
+    'question': 'What’s Garfield favourite food?',
+    'options': '1. Pizza\n2. Lasagna\n3. Burger\n4. Sandwich ',
+    'correct_option': '2'
+},
+     {
+    'question': 'How many years did Nelson Mandela spend in prison?',
+    'options': '1. 7\n2. 17\n3. 27\n4. 37',
+    'correct_option': '3'
+},
+     {
+    'question': 'How high is Mount Everest?',
+    'options': '1. 5,849\n2. 6,849 m\n3. 7,849 m\n4. 8,849 m',
+    'correct_option': '4'
+
+},
+     {
+    'question': 'Which chemical element has "Ag" as a symbol?',
+    'options': '1. Gold\n2. Silver\n3. Iron\n4. Carbon',
+    'correct_option': '2'
+}]
+
 
 def display_question():
     """
@@ -41,8 +94,8 @@ def check_user_answer():
 
     """
     6. if user input is valid check input with correct answer
-7. if user input == correct_option of the question then increment score
-"""
+    7. if user input == correct_option of the question then increment score
+    """
 
 
 def display_question_result():
