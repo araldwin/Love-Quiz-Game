74c00c92e6787c9da3caade8ac218406678d3657
araldwin <araldwin@gmail.com>
2022-10-13 20:52:26 +0000

    Add Data module to transfer my dictionary, import data module into run module, add initialize game function

---
+++ data.py
@@ -0,0 +1,57 @@
+QUESTIONS = [
+    {
+        "question": "In which Italian city can you find the Colosseum?",
+        "options": "1. Venice\n2. Rome\n3. Naples\n4. Milan",
+        "correct_option": "2",
+    },
+    {
+        "question": "In the Big Bang Theory Tv series, "
+        "what is the name of\nSheldon and Leonard’s neighbour?",
+        "options": "1. Penny\n2. Patty\n3. Lily\n4. Jessie",
+        "correct_option": "1",
+    },
+    {
+        "question": "In which country was the airline Ryanair founded?",
+        "options": "1. Germany\n2. Scotland\n3. Ireland\n4. Spain",
+        "correct_option": "3",
+    },
+    {
+        "question": "When was the first Harry Potter book published?",
+        "options": "1. 1997\n2. 1999\n3. 2001\n4. 2003",
+        "correct_option": "1",
+    },
+    {
+        "question": "“When I find myself in times of trouble, "
+        "Mother Mary comes to me”\nis the opening line of which song?",
+        "options": "1. Smells like teen spirit – Nirvana\n"
+        "2. Get lucky – Daft Punk\n"
+        "3. Sweet Child O’ Mine – Gun N’ Roses\n4. Let it be – The Beatles",
+        "correct_option": "4",
+    },
+    {
+        "question": "Who famously said “Veni, vidi, vici”?",
+        "options": "1. Winston Churchill\n2. Charles de Gaulle\n"
+        "3. Julius Caesar\n4. Alexander the Great",
+        "correct_option": "3",
+    },
+    {
+        "question": "What’s Garfield favourite food?",
+        "options": "1. Pizza\n2. Lasagna\n3. Burger\n4. Sandwich ",
+        "correct_option": "2",
+    },
+    {
+        "question": "How many years did Nelson Mandela spend in prison?",
+        "options": "1. 7\n2. 17\n3. 27\n4. 37",
+        "correct_option": "3",
+    },
+    {
+        "question": "How high is Mount Everest?",
+        "options": "1. 5,849 m\n2. 6,849 m\n3. 7,849 m\n4. 8,849 m",
+        "correct_option": "4",
+    },
+    {
+        "question": 'Which chemical element has "Ag" as a symbol?',
+        "options": "1. Gold\n2. Silver\n3. Iron\n4. Carbon",
+        "correct_option": "2",
+    },
+]
--- run.py
+++ run.py
@@ -7,65 +7,7 @@ import time
 import sys
 from pyfiglet import figlet_format
 from termcolor import colored
-
-
-QUESTIONS = [
-    {
-        "question": "In which Italian city can you find the Colosseum?",
-        "options": "1. Venice\n2. Rome\n3. Naples\n4. Milan",
-        "correct_option": "2",
-    },
-    {
-        "question": "In the Big Bang Theory Tv series, "
-        "what is the name of\nSheldon and Leonard’s neighbour?",
-        "options": "1. Penny\n2. Patty\n3. Lily\n4. Jessie",
-        "correct_option": "1",
-    },
-    {
-        "question": "In which country was the airline Ryanair founded?",
-        "options": "1. Germany\n2. Scotland\n3. Ireland\n4. Spain",
-        "correct_option": "3",
-    },
-    {
-        "question": "When was the first Harry Potter book published?",
-        "options": "1. 1997\n2. 1999\n3. 2001\n4. 2003",
-        "correct_option": "1",
-    },
-    {
-        "question": "“When I find myself in times of trouble, "
-        "Mother Mary comes to me”\nis the opening line of which song?",
-        "options": "1. Smells like teen spirit – Nirvana\n"
-        "2. Get lucky – Daft Punk\n"
-        "3. Sweet Child O’ Mine – Gun N’ Roses\n4. Let it be – The Beatles",
-        "correct_option": "4",
-    },
-    {
-        "question": "Who famously said “Veni, vidi, vici”?",
-        "options": "1. Winston Churchill\n2. Charles de Gaulle\n"
-        "3. Julius Caesar\n4. Alexander the Great",
-        "correct_option": "3",
-    },
-    {
-        "question": "What’s Garfield favourite food?",
-        "options": "1. Pizza\n2. Lasagna\n3. Burger\n4. Sandwich ",
-        "correct_option": "2",
-    },
-    {
-        "question": "How many years did Nelson Mandela spend in prison?",
-        "options": "1. 7\n2. 17\n3. 27\n4. 37",
-        "correct_option": "3",
-    },
-    {
-        "question": "How high is Mount Everest?",
-        "options": "1. 5,849 m\n2. 6,849 m\n3. 7,849 m\n4. 8,849 m",
-        "correct_option": "4",
-    },
-    {
-        "question": 'Which chemical element has "Ag" as a symbol?',
-        "options": "1. Gold\n2. Silver\n3. Iron\n4. Carbon",
-        "correct_option": "2",
-    },
-]
+from data import QUESTIONS
 
 QUESTION_INDEX = 0
 SCORE = 0
@@ -78,45 +20,51 @@ print(colored_ascii)
 time.sleep(1)
 
 
-while True:
-    name = input(colored("Please enter your username:\n", "green"))
-    if not re.match("^[a-z, A-Z, 0-9]*$", name):
-        print()
-        print(colored("Invalid username!"
-                      "No special characters allowed", "red"))
-    elif len(name) > 15:
-        print()
-        print(colored("Invalid! username too long!"
-                      "Only 15 characters allowed!", "red"))
-    elif name.strip() == '':
-        print(colored("Invalid username! You must enter [A-Z, 0-9]", "red"))
-    else:
-        break
-while True:
-    time.sleep(1)
-    print()
-    print(colored("Hello " + name + "!, are you ready to Start the quiz?!",
-          "yellow"))
-    print()
-    cont = input(colored("(start/exit?):\n", "green"))
-    while cont.lower() not in ("start", "exit"):
-        cont = input(colored("Type (start/exit?) only:\n", "green"))
-    if cont.lower() == "start":
+def initialize_game():
+
+    while True:
+        name = input(colored("Please enter your username:\n", "green"))
+        if not re.match("^[a-z, A-Z, 0-9]*$", name):
+            print()
+            print(colored("Invalid username!"
+                          "No special characters allowed", "red"))
+        elif len(name) > 15:
+            print()
+            print(colored("Invalid! username too long!"
+                          "Only 15 characters allowed!", "red"))
+        elif name.strip() == '':
+            print(colored("Invalid username!"
+                          "You must enter [A-Z, 0-9]", "red"))
+        else:
+            break
+    while True:
         time.sleep(1)
         print()
-        print(colored("loading...", "green"))
-        time.sleep(2)
+        print(colored("Hello " + name + "!, are you ready to Start the quiz?!",
+                      "yellow"))
         print()
-        print(colored("Proceeding to the first question...", "green"))
-        print()
+        cont = input(colored("(start/exit?):\n", "green"))
+        while cont.lower() not in ("start", "exit"):
+            cont = input(colored("Type (start/exit?) only:\n", "green"))
+        if cont.lower() == "start":
+            time.sleep(1)
+            print()
+            print(colored("loading...", "green"))
+            time.sleep(2)
+            print()
+            print(colored("Proceeding to the first question...", "green"))
+            print()
+            time.sleep(1)
+            break
         time.sleep(1)
-        break
-    time.sleep(1)
-    print()
-    print(colored("Knowledge is Power!!!", "yellow"))
-    print()
-    print(colored("Run program again whenever you are ready!", "red"))
-    sys.exit()
+        print()
+        print(colored("Knowledge is Power!!!", "yellow"))
+        print()
+        print(colored("Run program again whenever you are ready!", "red"))
+        sys.exit()
+
+
+initialize_game()
 
 
 def display_question(correct_question):
